398ebaa37c94027390c5267bb5bc97342d5e0763
Sean McIntyre <seanmcintyre@gmail.com>
2018-04-23 15:12:07 -0400

    npm updates

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function (config) {
       'text/x-typescript': ['ts','tsx']
     },
     remapIstanbulReporter: {
-      reports: {
+      dir: require('path').join(__dirname, 'coverage'), reports: {
         html: 'coverage',
         lcovonly: './coverage/coverage.lcov'
       }
