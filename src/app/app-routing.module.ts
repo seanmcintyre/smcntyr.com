3185f206a8b19aee09f3b6de16e85363b7cc7999
Sean McIntyre <seanmcintyre@gmail.com>
2018-04-24 15:34:45 -0400

    cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AboutComponent } from './about/about.component';
-import { ResumeComponent } from './resume/resume.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { LighthouseComponent } from './portfolio/lighthouse/lighthouse.component';
-
-
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   component: PortfolioComponent
-  // },
-  {
-    path: 'lighthouse',
-    component: LighthouseComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
-  {
-    path: 'resume',
-    component: ResumeComponent
-  },
-  {
-    path: 'lighthouse',
-    component: LighthouseComponent
-  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { LogosModule } from './logos/logos.module';
 
 import { AppComponent } from './app.component';
-import { ResumeComponent } from './resume/resume.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 import { AboutComponent } from './about/about.component';
-import { LighthouseComponent } from './portfolio/lighthouse/lighthouse.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClockComponent } from './clock/clock.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
-    ResumeComponent,
-    PortfolioComponent,
     AboutComponent,
-    LighthouseComponent,
     ClockComponent,
   ],
   imports: [
